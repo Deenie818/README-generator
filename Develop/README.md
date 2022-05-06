2807235e21555196e6de1bd6ea7a9422c9441b66
Deenie818 <99102981+Deenie818@users.noreply.github.com>
2022-05-05 21:07:45 -0700

    added markdown

--- Develop/README.md
+++ Develop/README.md
@@ -1,4 +1,27 @@
+
 # readme generator 
-[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+[![License](https://img.shields.io/badge/License-Mozilla Public License-blue.svg)](https://opensource.org/licenses/Mozilla Public License)
 ## Description 
-fjdlksj
+to simplify readme process
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+The following necessary dependancies must be installed to run the application
+## Usage
+In order to use this app, answer questions
+## License
+This project is licensed under the Mozilla Public License license.
+## Contributing
+Contributors: undefined
+## Tests
+To run tests, you need to run the following command: node index.js
+## Questions
+If you have any questions about the repo, please contact Deenie818 via e-mail: deenie818@yahoo.com
--- Develop/util/generateMarkdown.js
+++ Develop/util/generateMarkdown.js
@@ -17,12 +17,33 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(response) {
-  return `# ${response.Title} 
+  return `
+# ${response.Title} 
+
 [![License](https://img.shields.io/badge/License-${response.License}-blue.svg)](https://opensource.org/licenses/${response.License})
 ## Description 
 ${response.Description}
+
 ## Table of Contents
-- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+The following necessary dependancies must be installed to run the application
+## Usage
+In order to use this app, ${response.Usage}
+## License
+This project is licensed under the ${response.License} license.
+## Contributing
+Contributors: ${response.Contributing}
+## Tests
+To run tests, you need to run the following command: ${response.Tests}
+## Questions
+If you have any questions about the repo, please contact ${response.Github} via e-mail: ${response.Email}
 `;
 }
 
